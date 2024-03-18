0ce068c632aa63eda4c6754b387255599a350c65
Bilal Ladoui <bilal.ladoui@kuehne-nagel.com>
2024-03-18 11:38:31 +0200

    add subnets

--- main.tf
+++ main.tf
@@ -7,3 +7,44 @@ resource "aws_vpc" "it-nuggets-vpc" {
       Name = "it-nuggets-vpc"
     }
 }
+
+resource "aws_subnet" "public_subnet_1" {
+  vpc_id = aws_vpc.it-nuggets-vpc.id
+  cidr_block = "10.0.1.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = var.ZONE1
+  tags = {
+    Name = "it_nuggets_Public_Subnet_1"
+  }
+}
+
+resource "aws_subnet" "public_subnet_2" {
+  vpc_id = aws_vpc.it-nuggets-vpc.id
+  cidr_block = "10.0.2.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = var.ZONE2
+  tags = {
+    Name = "it_nuggets_Public_Subnet_2"
+  }
+}
+
+resource "aws_subnet" "private_subnet_1" {
+  vpc_id = aws_vpc.it-nuggets-vpc.id
+  cidr_block = "10.0.4.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = var.ZONE1
+  tags = {
+    Name = "it_nuggets_private_subnet_1"
+  }
+}
+
+
+resource "aws_subnet" "private_subnet_2" {
+  vpc_id = aws_vpc.it-nuggets-vpc.id
+  cidr_block = "10.0.5.0/24"
+  map_public_ip_on_launch = "true"
+  availability_zone = var.ZONE2
+  tags = {
+    Name = "private_subnet_2"
+  }
+}
