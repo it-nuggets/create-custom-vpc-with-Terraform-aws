f277489b733d7d7ec515f7c6b03f32966e8d2b92
Bilal Ladoui <bilal.ladoui@kuehne-nagel.com>
2024-03-18 12:50:33 +0200

    update the ec2 sg

--- sg.tf
+++ sg.tf
@@ -3,21 +3,29 @@ resource "aws_security_group" "terrafrom_sg" {
     description = "sg to allow ssh and port 80"
     vpc_id = aws_vpc.it-nuggets-vpc.id
 
+    ingress {
+    from_port   = 22  # SSH port
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    ingress {
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
+    egress {
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
+    }
+
     tags = {
       name = "Terraform__it_nuggets_project_sg"
     }
   
-}
-resource "aws_vpc_security_group_ingress_rule" "allow_tls_ipv4" {
-  security_group_id = aws_security_group.terrafrom_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  from_port         = 22
-  ip_protocol       = "tcp"
-  to_port           = 22
-}
-
-resource "aws_vpc_security_group_egress_rule" "allow_all_traffic_ipv4" {
-  security_group_id = aws_security_group.terrafrom_sg.id
-  cidr_ipv4         = "0.0.0.0/0"
-  ip_protocol       = "-1" # semantically equivalent to all ports
 }
