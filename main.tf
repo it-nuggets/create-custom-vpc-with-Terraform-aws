24190613c7ac040daf01c157753bde22900c5d32
Bilal Ladoui <bilal.ladoui@kuehne-nagel.com>
2024-03-18 11:47:04 +0200

    add IGW amd route tables

--- main.tf
+++ main.tf
@@ -48,3 +48,35 @@ resource "aws_subnet" "private_subnet_2" {
     Name = "private_subnet_2"
   }
 }
+
+resource "aws_internet_gateway" "IGW" {
+  vpc_id = aws_vpc.my_vpc.id
+  tags = {
+    Name = "IGW"
+  }
+}
+
+resource "aws_route_table" "public_RT" {
+  vpc_id = aws_vpc.my_vpc.id
+
+
+  route = {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.IGW.id
+  }
+  tags = {
+    Name = "Pulic_RT"
+  }
+}
+
+
+resource "aws_route_table_association" "public_subnet_1a" {
+  subnet_id = aws_subnet.public_subnet_1.id
+  route_table_id = aws_route_table.public_RT.id
+}
+
+
+resource "aws_route_table_association" "public_subnet_2b" {
+  subnet_id = aws_subnet.public_subnet_2.id
+  route_table_id = aws_route_table.public_RT.id
+}
